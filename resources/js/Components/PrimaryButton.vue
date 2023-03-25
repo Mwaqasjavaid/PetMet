c4d889d17d594707b76376d13b61c9a4c686f678
Mwaqasjavaid <mwaqasjavaid07@gmail.com>
2023-03-25 12:38:45 +0500

    first-comit

---
+++ resources/js/Components/PrimaryButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: 'submit',
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="
+            bg-blue-800
+            hover:bg-gray-900
+            text-white
+            font-bold
+            text-[23px]
+            p-2.5
+            rounded-lg
+        "
+    >
+        <slot />
+    </button>
+</template>
---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <div class="h-[100vh] flex flex-col items-center pt-[11px] bg-white">
+        <Link :href="route('login')">
+            <img width="200" src="/images/icons/Petmetlogo.jpg">
+        </Link>
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <slot />
+        </div>
+    </div>
+</template>
